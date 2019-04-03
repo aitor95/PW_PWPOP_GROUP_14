ab1841d6e31e56bb02e677463238edf62edda1f8
Joan Lluis Mudarra <jl.mudarra.2016@salleurl.edu>
2019-04-03 20:11:59 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$settings = require_once __DIR__ . '/../config/settings.php';
+$app = new \Slim\App($settings);
+
+require_once __DIR__ . '/../config/routes.php';
+
+$app->run();
+
