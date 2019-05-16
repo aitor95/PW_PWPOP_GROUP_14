77ed9b8fe58f3620aac3f673d165791446ff93f0
Aitor Blesa <aitor.blesa.2016@salleurl.edu>
2019-05-16 22:29:58 +0200

    Fix implementado validacion nupload product y por ello product overview tmbn. Falta añadir ruta correcta al boton

--- public/assets/js/upload.js
+++ public/assets/js/upload.js
@@ -6,52 +6,49 @@ var file = document.getElementById("file");
 
 function validaFormularioUpload(event){
     var devolver = true;
-    var imatge = document.getElementById("file_chooser").value;
-    var message_image = document.getElementById("message-chooser");
-   /* switch(imatge.substring(imatge.lastIndexOf('.') + 1).toLowerCase()){
-        case 'gif': case 'jpg': case 'png':
-            if (this.files[0].size/1024/1024 > 1 ){
-                alert(this.files[0].size);
-                message_image.innerText = "Invalid image format";
-                message_image.classList.add("error");
-                devolver = false;
-                $('#file_chooser').val('');
-            }
-            break;
-
-        default:
-            $('#file_chooser').val('');
-            alert("not an image");
-            message_image.innerHTML = "Invalid image format";
-            message_image.classList.add("error");
-            devolver = false;
-            break;
-    }*/
 
     var description = document.getElementById("description").value;
     var description_message = document.getElementById("message-description");
      if (description.length < 20){
-
          description_message.innerHTML = "Minimum 20 characters.";
          description_message.classList.add("error");
          devolver = false;
      } else {
          description_message.innerText = "";
          description_message.classList.remove("error");
-         devolver = true;
+
      }
 
-     var price = document.getElementById("price").value;
-     var price_message = document.getElementById("message-price");
-     if (isNaN(price_message)){
-         price_message.innerHTML =" Introduce a number";
-         price_message.classList.add("error");
-         devolver = false;
-     } else {
-         price_message.innerHTML ="";
-         price_message.classList.remove("error");
-         devolver = true;
-     }
+    var price = document.getElementById("price").value;
+    var price_message = document.getElementById("message-price");
+    if (isNaN(price)){
+        price_message.innerHTML =" Introduce a number";
+        price_message.classList.add("error");
+        devolver = false;
+    } else {
+        price_message.innerHTML = "";
+        price_message.classList.remove("error");
+    }
+
+    var imatge = document.getElementById("file_chooser");
+    var message_chooser = document.getElementById("message-chooser");
+    var imagePath = imatge.value;
+    var allowedExtensions = /(\.jpg|\.jpg|\.png|\.gif)$/i;
+    if (!allowedExtensions.test(imagePath)){
+        message_chooser.innerHTML = "Please, choose an image file";
+        message_chooser.classList.add("error");
+        devolver = false;
+    } else {
+        if (imatge.files[0].size/1024/1024 > 1){
+            message_chooser.innerHTML = "Choose an image maximum 1MB size";
+            message_chooser.classList.add("error");
+            devolver = false;
+        } else {
+            message_chooser.innerHTML = "";
+            message_chooser.classList.remove("error");
+        }
+    }
+
     if (!devolver){
         event.preventDefault();
     }
